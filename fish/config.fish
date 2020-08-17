1f82613e9f47f1f949124e4bd7a6371822b1060d
Sergey Bulavincev <sergey.bulavincev@ah.nl>
2020-08-17 10:34:05 +0300

    Set up fish git prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,19 @@
 # Initialize FZF keybindings
 fzf_key_bindings
+
 # Set terminal
 set -g TERM xterm-256color
+
 # Append paths
 set -a PATH "$HOME/bin/" "$HOME/.local/bin" "$HOME/.krew/bin" "$HOME/.rvm/bin/"
+
 # Set EDITOR to nvim
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
 # Set KUBECONFIG
 set -gx KUBECONFIG "$HOME/.kube/all-merged"
+
 # Do not complete CRD, to speed things up
 set -gx FISH_KUBECTL_COMPLETION_COMPLETE_CRDS 0
 
@@ -17,8 +21,17 @@ set -gx FISH_KUBECTL_COMPLETION_COMPLETE_CRDS 0
 load_keys
 
 # git_prompt
+# number of untracked files, dirty files, unpushed/unpulled commits, and more.
+set -g __fish_git_prompt_show_informative_status 1
+# set -g __fish_git_prompt_hide_untrackedfiles 1
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'no'
+set __fish_git_prompt_showupstream 'auto'
+set __fish_git_prompt_color_branch yellow
+
 # Configure FZF
 set -gx FZF_DEFAULT_COMMAND 'rg --files'
+
 # Smart fzf preview window
 set -gx FZF_DEFAULT_OPTS "--height 50% -1 --layout=reverse-list --multi \
   --preview='set -l data (file --mime {}); \
