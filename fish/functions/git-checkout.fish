a9ce39459b2e0af2f5c0c541d6507239487b9126
Sergey Bulavincev <sergey.bulavincev@ah.nl>
2020-08-21 08:29:43 +0300

    Fix git-checkout spaces

--- fish/functions/git-checkout.fish
+++ fish/functions/git-checkout.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function git-checkout --wraps='git checkout (git branch -l | fzf)' --description 'alias git-checkout=git checkout (git branch -l | fzf)'
-  git checkout (git branch -l | fzf) $argv;
+function git-checkout --wraps='git checkout (git branch -l | fzf)' --wraps='git checkout (git branch -l | fzf | tr -d "[:space:]")' --description 'alias git-checkout git checkout (git branch -l | fzf | tr -d "[:space:]")'
+  git checkout (git branch -l | fzf | tr -d "[:space:]") $argv;
 end
