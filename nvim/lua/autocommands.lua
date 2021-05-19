7fff9900caebfde0a3a1fd7a694e6a549245348b
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2021-05-19 09:13:36 +0300

    Remove curpos autocommand

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -6,17 +6,6 @@ local autocmds = {
     hl_yank = {
         {'TextYankPost',  '*', 'lua vim.highlight.on_yank{higroup="IncSearch", timeout=1500, on_visual=true}'},
     },
-    restore_curpos = {
-        {
-        'BufReadPost',
-        '*',
-        [[
-        if line("'\"") >= 1 && line("'\"") <= line("$")
-            exe "normal! g`\""
-        endif
-        ]],
-        },
-    },
 }
 
 require'utils'.nvim_create_augroups(autocmds)
