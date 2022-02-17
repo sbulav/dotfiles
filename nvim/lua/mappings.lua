d07d8ece5f46abeb1bcae922ff110b776eca3689
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2022-02-17 10:50:55 +0300

    fix(nvim): fix alt-e command triggering toggleterm

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -31,8 +31,8 @@ local opts = { noremap = true, silent = true }
 keymap("n", "K", "<CMD>lua Show_documentation()<CR>", opts)
 keymap("n", "_", "<CMD>lua _K9S_TOGGLE()<CR>", opts)
 keymap("t", "_", "<CMD>lua _K9S_TOGGLE()<CR>", opts)
-keymap("n", "|", "<cmd>ToggleTerm direction=float<CR>", opts)
-keymap("t", "|", "<cmd>ToggleTerm<CR>", opts)
+keymap("n", "<M-\\>", "<cmd>ToggleTerm direction=float<CR>", opts)
+keymap("t", "<M-\\>", "<cmd>ToggleTerm<CR>", opts)
 
 --- luasnip keymappings
 keymap("i", "<c-x>", "<cmd>lua require'luasnip'.expand_or_jump()<CR>", opts)
