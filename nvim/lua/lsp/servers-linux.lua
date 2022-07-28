b00bb38fd987f7561f393c9f616aa0a2f8131390
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2022-07-28 11:22:47 +0300

    fix(nvim): lsp capabilities for linux

--- nvim/lua/lsp/servers-linux.lua
+++ nvim/lua/lsp/servers-linux.lua
@@ -7,7 +7,10 @@ local utils = require "utils"
 
 -- tell lsp about nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if cmp_nvim_lsp_ok then
+    capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+end
 
 utils.info("Initilizing LSP server in containers", "LSP")
 require("lspconfig").pyright.setup {
