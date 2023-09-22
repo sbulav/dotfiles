e9a1b4414dfb3e8615a97ec1ee1aeffa4a98c222
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2023-09-22 22:02:34 +0300

    nix: fix nixos imports

--- nix/flake.nix
+++ nix/flake.nix
@@ -95,20 +95,19 @@
         modules = [./nixos/configuration.nix];
       };
     };
+    imports = [
+      # Import home-manager's NixOS module
+      inputs.home-manager.nixosModules.home-manager
+    ];
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      "sab@nz" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs;}; # Pass flake inputs to our config
+        # > Our main home-manager configuration file <
+        modules = [./home-manager/home.nix];
+      };
+    };
   };
-
-  # imports = [
-  #   # Import home-manager's NixOS module
-  #   inputs.home-manager.nixosModules.home-manager
-  # ];
-  # # Standalone home-manager configuration entrypoint
-  # # Available through 'home-manager --flake .#your-username@your-hostname'
-  # homeConfigurations = {
-  #   "sab@nz" = home-manager.lib.homeManagerConfiguration {
-  #     pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-  #     extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-  #     # > Our main home-manager configuration file <
-  #     modules = [ ./home-manager/home.nix ];
-  #   };
-  # };
 }
