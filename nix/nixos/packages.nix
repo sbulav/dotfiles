1160744dc8d766775e73a246a23bdf7a1deb4b2b
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2023-09-25 11:46:48 +0300

    feat(nixos): work on packages

--- nix/nixos/packages.nix
+++ nix/nixos/packages.nix
@@ -1,26 +1,26 @@
-{pkgs}:
-with pkgs; let
-  shared-packages = import ../shared/packages.nix {inherit pkgs;};
-in
-  shared-packages
-  ++ [
-    adwaita-qt
-    adwaita-qt6
-    gnome.gnome-tweaks
-    gnumake
-    home-manager
-    hyprland-protocols
-    hyprpaper
-    hyprpicker
-    pamixer
-    polkit_gnome
-    qt5.qtwayland
-    qt6.qmake
-    qt6.qtwayland
-    vlc
-    xdg-desktop-portal
-    xdg-desktop-portal-gtk
-    xdg-desktop-portal-hyprland
-    xdg-utils
-    zathura # PDF viewer
-  ]
+{pkgs, ...}: {
+  # Load configuration that is shared across systems
+  environment.systemPackages = with pkgs;
+    [
+      adwaita-qt
+      adwaita-qt6
+      gnome.gnome-tweaks
+      gnumake
+      home-manager
+      hyprland-protocols
+      hyprpaper
+      hyprpicker
+      pamixer
+      polkit_gnome
+      qt5.qtwayland
+      qt6.qmake
+      qt6.qtwayland
+      vlc
+      xdg-desktop-portal
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-hyprland
+      xdg-utils
+      zathura # PDF viewer
+    ]
+    ++ (import ../shared/packages.nix {inherit pkgs;});
+}
