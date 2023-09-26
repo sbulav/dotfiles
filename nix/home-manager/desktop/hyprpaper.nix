98db9a5ca2aff373c76be8979f1003b20eb7969a
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2023-09-26 14:13:12 +0300

    feat(nix): set up hyprpaper for wallpapers

---
+++ nix/home-manager/desktop/hyprpaper.nix
@@ -0,0 +1,10 @@
+let
+  wallpaper = ./wallpapers/2.jpg;
+in {
+  xdg.configFile."hypr/hyprpaper.conf".text = ''
+    preload = ${wallpaper}
+    wallpaper = eDP-1,${wallpaper}
+    wallpaper = DP-1,${wallpaper}
+    wallpaper = DP-2,${wallpaper}
+  '';
+}
