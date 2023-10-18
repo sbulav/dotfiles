370fb6554856d670f35fe98ec18ad703ea8f762e
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2023-10-18 12:10:21 +0300

    feat(nix): auth with fingerprint

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -154,9 +154,12 @@
     symbolsFile = ../symbols/colemak_dh;
   };
 
+  services.fprintd.enable = true;
+
   security.rtkit.enable = true;
   security.sudo.wheelNeedsPassword = false;
   security.pam.services.swaylock = {};
+  security.pam.services.swaylock.fprintAuth = true;
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
