ecc0af055643c4282f874a71acefdff30bef81eb
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2024-01-15 12:44:01 +0300

    fix(nix): enable 2 ways of auth in swaylock

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -166,8 +166,25 @@
 
   security.rtkit.enable = true;
   security.sudo.wheelNeedsPassword = false;
-  security.pam.services.swaylock = {};
-  security.pam.services.swaylock.fprintAuth = true;
+  # security.pam.services.swaylock.fprintAuth = true;
+  security.pam.services.swaylock.text = ''
+    # Account management.
+    account required pam_unix.so
+
+    # Authentication management.
+
+    # prompt for a password; pressing Enter on a blank field will proceed to fingerprint authentication.
+    auth sufficient pam_unix.so nullok likeauth try_first_pass
+    auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so
+    auth required pam_deny.so
+
+    # Password management.
+    password sufficient pam_unix.so nullok sha512
+
+    # Session management.
+    session required pam_env.so conffile=/etc/pam/environment readenv=0
+    session required pam_unix.so
+  '';
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
