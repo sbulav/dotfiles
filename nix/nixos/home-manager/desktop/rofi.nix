993cf0591358abbcc869178ea8c57e5721e86a25
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2024-01-22 17:56:05 +0300

    feat(nix): add vpn selector rofi

--- nix/nixos/home-manager/desktop/rofi.nix
+++ nix/nixos/home-manager/desktop/rofi.nix
@@ -12,11 +12,11 @@
       display-drun = "   Apps ";
       display-run = "   Run ";
       display-window = " 﩯  Window";
+      display-vpn = " VPN ";
       drun-display-format = "{icon} {name}";
-      hide-scrollbar = true;
       icon-theme = "Fluent";
       location = 0;
-      modi = "combi,drun,calc,";
+      modi = "run,drun,vpn:~/.config/rofi/rofi-vpn.sh,calc,";
       show-icons = true;
       sidebar-mode = true;
       sorting-method = "fzf";
@@ -29,5 +29,80 @@
       matching = "fuzzy";
     };
     theme = ./rofi/catppuccin-frappe.rasi;
+  };
+  home.file.".config/rofi/rofi-vpn.sh" = {
+    executable = true;
+    text = ''
+      #!/usr/bin/env sh
+
+        multiple=false
+        active_marker='[*] '
+
+        error() {
+            echo "''${@}" >&2
+            exit 1
+        }
+
+        maybe_remove_active_marker() {
+            length=''$(printf '%s' "''${active_marker}" | wc -c)
+            if [ "''$(echo "''${1}" | cut -c "-''${length}")" = "''${active_marker}" ]; then
+                echo "''${1}" | cut -c ''$((length + 1))-
+                return 0
+            fi
+            echo "''${1}"
+            return 1
+        }
+
+        disconnect_all() {
+          active_connections=$(
+              nmcli --get-values name,type connection show --active \
+              | awk -F ':' '
+              $2 != "vpn" && $2 != "wireguard" { next }
+              { print $1 }
+              '
+              )
+            if [ -z "''${active_connections}" ]; then
+              return
+                fi
+                echo "''${active_connections}" | xargs -d '\n' nmcli connection down \
+                > /dev/null
+        }
+
+        connection_list() {
+            nmcli --get-values name,type,active,state connection show \
+            | awk -F ':' '
+                ''$2 != "vpn" && ''$2 != "wireguard" { next }
+                ''$3 == "yes" { ''$1 = "\a" ''$1 }
+                { print ''$1 }
+            ' \
+            | LC_COLLATE=C sort \
+            | sed -n "s/^\a/''${active_marker}/;p"
+        }
+
+        toggle_connection() {
+            if connection=''$(maybe_remove_active_marker "''${1}"); then
+                action='down'
+            else
+                action='up'
+            fi
+            if [ ''${multiple} != true ]; then
+                disconnect_all
+                if [ ''${action} = 'down' ]; then
+                    return
+                fi
+            fi
+            nmcli connection "''${action}" "''${connection}" > /dev/null &
+        }
+
+        run_rofi_mode() {
+            if [ ''${#} -eq 0 ]; then
+                connection_list
+            else
+                toggle_connection "''${1}"
+            fi
+        }
+
+        "run_rofi_mode" "''${@}"
+    '';
   };
 }
