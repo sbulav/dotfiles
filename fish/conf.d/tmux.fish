65b1a5673c70f3ace87b80fc2bf4b84dc7aaefdd
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2024-01-29 15:41:18 +0300

    feat(tmux): don't auto start session

--- fish/conf.d/tmux.fish
+++ fish/conf.d/tmux.fish
@@ -1,5 +1,5 @@
 if not set -q TMUX; and status --is-interactive;
     set -g TMUX tmux new-session -d -s base
     eval $TMUX
-    tmux attach-session -d -t base
+    # tmux attach-session -d -t base
 end
