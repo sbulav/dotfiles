fa31d5e25f9abd147550b4e2bd3b9547d792d415
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2024-08-05 15:59:20 +0300

    fixup

--- nix/systems/x86_64-linux/serverz/hardware-configuration.nix
+++ nix/systems/x86_64-linux/serverz/hardware-configuration.nix
@@ -35,7 +35,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
+  networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
