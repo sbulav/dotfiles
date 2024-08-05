04c1ffe6aae9bb6e56fa6d996e53aff13c344e7b
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2024-08-05 14:26:40 +0300

    fixup

--- nix/systems/x86_64-linux/serverz/default.nix
+++ nix/systems/x86_64-linux/serverz/default.nix
@@ -11,13 +11,13 @@ in {
   ];
 
   # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub = {
-    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
-    # devices = [ ];
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-  };
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub = {
+  #   # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+  #   devices = [ ];
+  #   efiSupport = true;
+  #   efiInstallAsRemovable = true;
+  # };
 
   # environment.systemPackages = with pkgs; [
   #   # Any particular packages only for this host
