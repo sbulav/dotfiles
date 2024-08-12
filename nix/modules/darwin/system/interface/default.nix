2977ced02d07e54ba82598998660c0dcf0633cd5
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2024-08-12 12:13:39 +0300

    feat(nix): add more default darwin options

--- nix/modules/darwin/system/interface/default.nix
+++ nix/modules/darwin/system/interface/default.nix
@@ -15,22 +15,54 @@ in {
 
   config = mkIf cfg.enable {
     system.defaults = {
-      # dock.autohide = true;
-
       LaunchServices = {
         LSQuarantine = false;
       };
 
       finder = {
         AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        CreateDesktop = false;
+        FXDefaultSearchScope = "SCcf";
         FXEnableExtensionChangeWarning = false;
+        # NOTE: Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
+        FXPreferredViewStyle = "Nlsv";
+        QuitMenuItem = true;
+        ShowStatusBar = true;
         _FXShowPosixPathInTitle = true;
       };
 
-      # NSGlobalDomain = {
-      #   _HIHideMenuBar = true;
-      #   AppleShowScrollBars = "Always";
-      # };
+      CustomSystemPreferences = {
+        finder = {
+          DisableAllAnimations = true;
+          ShowExternalHardDrivesOnDesktop = false;
+          ShowHardDrivesOnDesktop = false;
+          ShowMountedServersOnDesktop = false;
+          ShowRemovableMediaOnDesktop = false;
+          _FXSortFoldersFirst = true;
+        };
+
+        NSGlobalDomain = {
+          AppleAccentColor = 1;
+          AppleHighlightColor = "0.65098 0.85490 0.58431";
+          AppleSpacesSwitchOnActivate = false;
+          WebKitDeveloperExtras = true;
+        };
+      };
+
+      # login window settings
+      loginwindow = {
+        # disable guest account
+        GuestEnabled = false;
+        # show name instead of username
+        SHOWFULLNAME = false;
+      };
+
+      screencapture = {
+        disable-shadow = true;
+        location = "$HOME/Pictures/screenshots/";
+        type = "png";
+      };
     };
   };
 }
