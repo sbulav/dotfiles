075d2ab05d08e497328e38430c6e7833fa047343
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2024-08-14 09:28:34 +0300

    feat(nvim): add explicit yazi keymaps

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -21,6 +21,17 @@ return {
             end,
             desc = "Open the file manager",
         },
+        keymaps = {
+            show_help = "<f1>",
+            open_file_in_vertical_split = "<c-v>",
+            open_file_in_horizontal_split = "<c-x>",
+            open_file_in_tab = "<c-t>",
+            grep_in_directory = "<c-s>",
+            replace_in_directory = "<c-g>",
+            cycle_open_buffers = "<tab>",
+            copy_relative_path_to_selected_files = "<c-y>",
+            send_to_quickfix_list = "<c-q>",
+        },
         {
             -- Open in the current working directory
             "<leader>cw",
