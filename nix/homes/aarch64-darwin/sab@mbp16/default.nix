e2634772486517b64b9607567ccb753c9a2f49e2
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2024-09-13 10:18:28 +0300

    feat: enable obsidian in mac16

--- nix/homes/aarch64-darwin/sab@mbp16/default.nix
+++ nix/homes/aarch64-darwin/sab@mbp16/default.nix
@@ -10,6 +10,7 @@ with lib.custom; {
       name = config.snowfallorg.user.name;
     };
     apps = {
+      obsidian = enabled;
       zoom-us = enabled;
     };
 
--- nix/modules/darwin/apps/obsidian/default.nix
+++ nix/modules/darwin/apps/obsidian/default.nix
@@ -7,7 +7,7 @@ with lib;
 with lib.custom; let
   cfg = config.custom.apps.obsidian;
 in {
-  options.custom.desktop.obsidian = {
+  options.custom.apps.obsidian = {
     enable = mkEnableOption "Enable Obsidian note-taking app";
   };
 
