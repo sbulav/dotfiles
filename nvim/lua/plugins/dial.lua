734d841ac4b7e5f7a48bed35c39e6d92763c6fcd
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2024-10-03 16:05:16 +0300

    feat(nvim): dial.nvim - add enabled/disabled to cycle

--- nvim/lua/plugins/dial.lua
+++ nvim/lua/plugins/dial.lua
@@ -1,23 +1,26 @@
 -- better increase/descrease
 return {
-  "monaqa/dial.nvim",
+    "monaqa/dial.nvim",
   -- stylua: ignore
+  -- false
   keys = {
     { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
     { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
   },
-  config = function()
-    local augend = require("dial.augend")
-    require("dial.config").augends:register_group({
-      default = {
-        augend.integer.alias.decimal,
-        augend.integer.alias.hex,
-        augend.date.alias["%Y/%m/%d"],
-        augend.constant.alias.bool,
-        augend.semver.alias.semver,
-        augend.constant.new({ elements = { "let", "const" } }),
-        augend.constant.new({ elements = { "True", "False" } }),
-      },
-    })
-  end,
+    config = function()
+        local augend = require "dial.augend"
+        require("dial.config").augends:register_group {
+            default = {
+                augend.integer.alias.decimal,
+                augend.integer.alias.hex,
+                augend.date.alias["%Y/%m/%d"],
+                augend.constant.alias.bool,
+                augend.semver.alias.semver,
+                augend.constant.new { elements = { "let", "const" } },
+                augend.constant.new { elements = { "True", "False" } },
+                augend.constant.new { elements = { "enabled", "disabled" } },
+                augend.constant.new { elements = { "enable", "disable" } },
+            },
+        }
+    end,
 }
