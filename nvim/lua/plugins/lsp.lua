8004c7acb9e3bfd45ab48019b0c8320b224e001e
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2024-10-24 14:49:54 +0300

    feat(nix): Add LSP documentation on nixpkgs and flake

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -122,24 +122,33 @@ return {
                 end,
                 -- manually configured servers
                 require("lspconfig")["helm_ls"].setup {},
+
                 require("lspconfig").nixd.setup {
                     cmd = { "nixd" },
                     settings = {
                         nixd = {
                             nixpkgs = {
-                                expr = "import <nixpkgs> { }",
+                                -- For flake.
+                                -- This expression will be interpreted as "nixpkgs" toplevel
+                                -- Nixd provides package, lib completion/information from it.
+                                -- Resource Usage: Entries are lazily evaluated, entire nixpkgs takes 200~300MB for just "names".
+                                -- Package documentation, versions, are evaluated by-need.
+                                expr = "import (builtins.getFlake(toString ./.)).inputs.nixpkgs { }",
                             },
                             formatting = {
                                 command = { "alejandra" }, -- or nixfmt or nixpkgs-fmt
                             },
-                            -- options = {
-                            --   nixos = {
-                            --       expr = '(builtins.getFlake "/PATH/TO/FLAKE").nixosConfigurations.CONFIGNAME.options',
-                            --   },
-                            --   home_manager = {
-                            --       expr = '(builtins.getFlake "/PATH/TO/FLAKE").homeConfigurations.CONFIGNAME.options',
-                            --   },
-                            -- },
+                            options = {
+                                nixos = {
+                                    expr = "let flake = builtins.getFlake(toString ./.); in flake.nixosConfigurations.nz.options",
+                                },
+                                home_manager = {
+                                    expr = 'let flake = builtins.getFlake(toString ./.); in flake.homeConfigurations."sab@mbp16".options',
+                                },
+                                darwin = {
+                                    expr = "let flake = builtins.getFlake(toString ./.); in flake.darwinConfigurations.mbp16.options",
+                                },
+                            },
                         },
                     },
                 },
@@ -196,6 +205,7 @@ return {
                 -- "pyright",
                 -- "black",
                 -- "marksman",
+                -- "nixd"
                 "lua-language-server",
             },
         },
