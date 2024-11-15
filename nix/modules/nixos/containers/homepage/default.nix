8aa5644325a05bff2bf0529766d17a753af5a815
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2024-11-15 17:38:38 +0300

    feat(nix): make dns rewrites in module

--- nix/modules/nixos/containers/homepage/default.nix
+++ nix/modules/nixos/containers/homepage/default.nix
@@ -22,6 +22,8 @@ in {
         host = "${cfg.host}";
         url = "http://${cfg.localAddress}:8082";
       })
+    (import ../shared/shared-adguard-dns-rewrite.nix
+      {host = "${cfg.host}";})
   ];
   config = mkIf cfg.enable {
     containers.homepage = {
@@ -123,14 +125,5 @@ in {
         system.stateVersion = "24.11";
       };
     };
-
-    containers.adguard.config.services.adguardhome.settings.filtering.rewrites =
-      lib.mkIf config.${namespace}.containers.adguard.enable
-      [
-        {
-          domain = "${cfg.host}";
-          answer = "${config.${namespace}.containers.adguard.rewriteAddress}";
-        }
-      ];
   };
 }
