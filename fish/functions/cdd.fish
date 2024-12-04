d118fa870cc49382c0e98c0c130cd117f90a2aaf
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2024-12-04 10:08:48 +0300

    feat!: extract nix config to dotnix

--- fish/functions/cdd.fish
+++ fish/functions/cdd.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function cdd --wraps='~/dotfiles/nix' --wraps='cd ~/dotfiles/nix' --description 'alias cdd=cd ~/dotfiles/nix'
+function cdd --wraps='~/dotnix' --wraps='cd ~/dotnix' --description 'alias cdd=cd ~/dotnix'
   cd ~/dotfiles/nix $argv;
 end
--- nix/overlays/zoom-us/default.nix
+++
@@ -1,4 +0,0 @@
-{channels, ...}: final: prev: {
-  # TODO: remove this overlay when screen sharing if fixed in 6.1.15+
-  inherit (channels.stable) zoom-us;
-}
