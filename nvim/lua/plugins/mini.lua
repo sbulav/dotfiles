ccc89a7d52d3429331409a761796aab435127544
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2025-01-09 10:58:08 +0300

    feat(comments): migrate to mini.comment

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -66,6 +66,35 @@ return {
                 join = "",
             },
         }
+        require("mini.comment").setup {
+            options = {
+                -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#minicomment
+                custom_commentstring = function()
+                    return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+                end,
+                ignore_blank_line = true,
+                -- Whether to recognize as comment only lines without indent
+                start_of_line = false,
+                -- Whether to force single space inner padding for comment parts
+                pad_comment_parts = true,
+            },
+
+            mappings = {
+                -- Toggle comment (like `gcip` - comment inner paragraph) for both
+                -- Normal and Visual modes
+                comment = "gc",
+
+                -- Toggle comment on current line
+                comment_line = "gcc",
+
+                -- Toggle comment on visual selection
+                comment_visual = "gc",
+
+                -- Define 'comment' textobject (like `dgc` - delete whole comment block)
+                -- Works also in Visual mode if mapping differs from `comment_visual`
+                textobject = "gc",
+            },
+        }
         require("mini.surround").setup {
             -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
             highlight_duration = 900,
