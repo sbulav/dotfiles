270a67eadf6c3558a70e7f342f165d683c3f2449
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2025-01-21 12:06:04 +0300

    fix(lazydev): add libuv

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,7 +4,16 @@ return {
         event = "BufReadPre",
         dependencies = {
             { "saghen/blink.cmp" },
-            { "folke/lazydev.nvim", ft = "lua", opts = {} },
+            {
+                "folke/lazydev.nvim",
+                ft = "lua",
+                opts = {
+                    library = {
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                        { path = "snacks.nvim", words = { "Snacks" } },
+                    },
+                },
+            },
             "mason.nvim",
             "williamboman/mason-lspconfig.nvim",
             -- "hrsh7th/cmp-nvim-lsp",
