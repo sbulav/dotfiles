809f0311d3625bda960d4da0c4a1e4da3fd425e8
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2025-01-24 09:57:02 +0300

    fix(fish): allow posix-source to handle env values with =

--- fish/functions/posix-source.fish
+++ fish/functions/posix-source.fish
@@ -1,8 +1,11 @@
 function posix-source --description 'Set env variables like in POSIX'
   if status --is-login
-    for i in (cat $argv)
-      set arr (echo $i |tr = \n)
-        set -gx $arr[1] $arr[2]
+    for line in (cat $argv)
+      # Split only on the first '='
+      set key (echo $line | cut -d '=' -f 1)
+      set value (echo $line | cut -d '=' -f 2-)
+      # Export the variable
+      set -gx $key $value
     end
   end
 end
