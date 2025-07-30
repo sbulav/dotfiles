a41ab6edfc52ed5c0e679758517cc2af73fcc517
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2025-07-30 16:05:12 +0300

    feat(neovim): switch to new version of nredir

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -10,7 +10,25 @@ return {
     { "janko/vim-test", cmd = { "TestNearest", "TestFile", "TestSuite", "TestLast", "TestVisit" } },
 
     -- Redirect output to scratch buffer
-    "sbulav/nredir.nvim",
+    {
+
+        "sbulav/nredir.nvim",
+        -- dir = "~/git_priv/nredir.nvim", -- local checkout
+        cmd = "Nredir", -- lazy-load when :Nredir is used
+        config = function()
+            -- call setup (you can omit the table if you want all defaults)
+            require("nredir").setup {
+                -- override the split command if you like:
+                -- split_cmd = "botright vsplit",
+                --
+                -- override the keymaps in the Nredir buffer:
+                -- keymaps = { close = "q", zoom = "<CR>", wrap = "w" },
+                --
+                -- speed up or slow down the spinner:
+                -- spinner_interval = 100,
+            }
+        end,
+    },
 
     -- Lint Jenkins files
     -- { "ckipp01/nvim-jenkinsfile-linter", dependencies = { "nvim-lua/plenary.nvim" }, ft = "groovy" },
