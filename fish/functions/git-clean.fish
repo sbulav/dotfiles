f202b809f095fbac23f7eea7dc3cf929cd415977
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2026-04-23 08:26:57 +0300

    fix: actualize git-clean

--- fish/functions/git-clean.fish
+++ fish/functions/git-clean.fish
@@ -1,4 +1,9 @@
-# Defined in - @ line 1
 function git-clean --wraps=git\ branch\ --merged\ \|\ egrep\ -v\ \"\(\^\\\*\|master\|dev\)\"\ \|\ xargs\ git\ branch\ -d\;git\ remote\ prune\ origin --description alias\ git-clean=git\ branch\ --merged\ \|\ egrep\ -v\ \"\(\^\\\*\|master\|dev\)\"\ \|\ xargs\ git\ branch\ -d\;git\ remote\ prune\ origin
-  git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d;git remote prune origin $argv;
+  argparse 'f/force' -- $argv; or return
+  set -l del_flag -d
+  if set -q _flag_force
+    set del_flag -D
+  end
+  git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch $del_flag
+  git remote prune origin $argv
 end
